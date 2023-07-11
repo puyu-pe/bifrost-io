62164eabf0a683bd8ae4e7bf33651659c7c778aa
DeveloperOscar <161139@unamba.edu.pe>
2023-07-11 16:36:15 -0500

    Se refactorizo parte del codigo para produccion

--- index.js
+++ index.js
@@ -25,30 +25,30 @@ io.sockets.setMaxListeners(MAX_LISTENERS);
 
 instrument(io, {
 	auth: false,
-	mode: "development"
+	mode: "production"
 });
 
 
 const storage = new BifrostPrinterStorage(EXPIRES_TIME_FROM_DATA);
+const configuredNamespaces = new Set();
 
-const setNamespace = new Set();
 io.on("connection", socket => {
-	log(`Cliente conectado: \n\tip: ${socket.handshake.address} \n\tsocket_id:${socket.id}`, "io.on('connection')");
+	log(`Nueva Conexión: \n\t\tip-address -> ${socket.handshake.address} \n\t\tsocket-id -> ${socket.id}`, "io event: 'connection'");
 
 	socket.on("yures-printer", data => {
 		if (!data || !data.namespace) {
-			log(`Warning: \n\tEl cliente ${data.clientname} no envio información necesaria para configurar la conexión.`, "socket.on('yures-printer')");
+			log(`Warning: \n\tEl cliente ${data.clientname} no envio información necesaria para configurar la conexión.`, "socket event: 'yures-printer'");
 			socket.emit("yures-printer-status", {
 				message: `Fallo al conectarse al namespace: '${data.namespace}' namespace incorrecto`,
 				status: "error",
 			});
 			return;
 		}
-		log(`El Cliente ${data.clientname || "No Specified"} conectado a ${data.namespace} `, 'socket.on("yures-printer")');
-		if (!setNamespace.has(data.namespace)) {
+		log(`Una instancia de ${data.clientname || "No Specified"} conectado con ${data.namespace} `, 'socket event: "yures-printer"');
+		if (!configuredNamespaces.has(data.namespace)) {
 			const namespace = io.of(data.namespace);
-			configWorkflowYuresPrinter(namespace);
-			setNamespace.add(data.namespace);
+			configWorkflowYuresPrinter(namespace, data.clientname);
+			configuredNamespaces.add(data.namespace);
 		}
 		socket.emit("yures-printer-status", {
 			message: "Namespace configurado exitosamente",
@@ -61,26 +61,26 @@ io.on("connection", socket => {
 /**
  * @param {Namespace} namespace - namespace socket.io
  */
-function configWorkflowYuresPrinter(namespace) {
+function configWorkflowYuresPrinter(namespace, clientname) {
 	namespace.on("connection", (socket) => {
 
-		log(`NAMESPACE: ${namespace.name} ACTIVO`, "namespace.on('connection')");
+		log(`"${namespace.name}" ha entrado en actividad`, "namespace-event: 'connection'");
 
 		socket.on("yures:save-print", async (dataToPrint) => {
 			try {
 				log(`Se recibio información para imprimir de ${namespace.name}`, "namespace-event: yures:save_print");
 				const storageInfo = await storage.enqueue(namespace.name, dataToPrint);
 				if (storageInfo.success) {
-					log(`Se almaceno información para imprimir en ${namespace.name}`, "namespace-event: yures:save-print");
+					log(`Se almaceno información de ${namespace.name} listo para imprimir`, "namespace-event: yures:save-print");
 					namespace.emit("printer:to-print", { data: storageInfo.memObject });
 					namespace.emit("yures:save-print-status", { message: "Impresion almacenada correctamente", success: "success" });
 				}
 				else {
-					log(`Danger:\n\t hubo un fallo al almacenar información en ${namespace.name}`, "namespace-event: yures:save-print");
+					log(`Danger:\n\tFallo al almacenar información de ${namespace.name}`, "namespace-event: yures:save-print");
 					namespace.emit("yures:save-print-status", { message: "No se persistio la información", success: "error" });
 				}
 			} catch (error) {
-				log(`error en ejecución en : ${error.toString()}`, "namespace-event: yures:save-print");
+				log(`error en ejecución: ${error.toString()}`, "namespace-event: yures:save-print");
 				namespace.emit("yures:save-print-status", { message: "El servidor a fallado, vuelve a intentarlo despues.", success: "error" });
 			}
 		})
@@ -108,17 +108,42 @@ function configWorkflowYuresPrinter(namespace) {
 			}
 		})
 
+		socket.on("disconnect", () => {
+			log(`Una instancia de ${clientname} en el namespace ${namespace.name} se desconecto!!`, "namespace-event: disconnect");
+		})
+
 	})
 }
+
+httpServer.on("onerror", (error) => {
+	log(`A ocurrido un error en el servidor: ${error}`, "httpServer.on('onerror')");
+})
 
 
 let count_log = 1;
 function log(message, scope) {
-	console.log(`\nN°${count_log} LOG-REGISTER AT ${new Date().toString()}:\n`);
-	console.log(`\tscope: ${scope || 'Not Specified'} \n\tlog: ${message || 'Not Specified'}\n`);
-	console.log(`############# ---end---  ##############\n`);
+	console.log(`\n---------------------------- LOG N° ${count_log} ----------------------------`);
+	console.log(`Hora: ${formatDate(new Date())}`);
+	console.log(`\tinfo: ${message || ' '}`);
+	console.log(`\tscope: ${scope || 'scope no especificado'}`);
+	console.log(`--------------------------------- end -----------------------------------------\n`);
 	++count_log;
 };
+
+
+
+/**
+ * @param {Date} date - namespace socket.io
+ */
+function formatDate(date) {
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+	const hours = String(date.getHours()).padStart(2, '0');
+	const minutes = String(date.getMinutes()).padStart(2, '0');
+	const seconds = String(date.getSeconds()).padStart(2, '0');
+	return `${hours}:${minutes}:${seconds} - ${day}-${month}-${year}`;
+}
 
 httpServer.listen(SERVER_PORT, () => {
 	console.log(`Bifrost habilitado en el puerto ${SERVER_PORT}!!!`);
