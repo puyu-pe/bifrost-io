30a5df0d8d913f2e1e79cc733379faaaaea1ceed
OsLan99 <161139@unamba.edu.pe>
2023-08-10 12:02:52 -0500

    FEAT implementa mutex a printer:printed
    ahora se accede de manera concurrente al objeto storage gracias a la
    libreria async-mutex
    
    issue #6

--- memcached/printer-storage.js
+++ memcached/printer-storage.js
@@ -1,8 +1,6 @@
-const { Client } = require('memjs');
-
+const { Client } = require("memjs");
 
 class BifrostPrinterStorage {
-
   constructor(timeExpires) {
     this.memcached = Client.create();
     this.timeExpires = timeExpires;
@@ -15,33 +13,40 @@ class BifrostPrinterStorage {
       tickets: JSON.stringify(dataToPrint),
       created_at: new Date(created_at).toString(),
       namespace,
-    }
+    };
     const memObject = {};
     memObject[`${created_at}`] = JSON.stringify(register);
     //devuelve true si almaceno correctamente la data
-    const success = await this.memcached.set(namespace, JSON.stringify({ ...queue, ...memObject }), { expires: this.timeExpires });
+    const success = await this.memcached.set(
+      namespace,
+      JSON.stringify({ ...queue, ...memObject }),
+      { expires: this.timeExpires }
+    );
     return { success, memObject };
   }
 
   async dequeue(namespace, created_at) {
     const queue = await this.getQueue(namespace);
-    const memObject = queue[created_at];
-    delete queue[created_at];
-    return memObject && await this.memcached.set(namespace, JSON.stringify({ ...queue }), { expires: this.timeExpires });
+    const existsForDelete = queue.hasOwnProperty(created_at);
+    if (existsForDelete) {
+      delete queue[created_at];
+      this.memcached.set(namespace, JSON.stringify({ ...queue }), {
+        expires: this.timeExpires,
+      });
+    }
+    return existsForDelete;
   }
 
   async getQueue(namespace) {
     let queue = {};
     const memData = await this.memcached.get(namespace);
-    if (memData.value)
-      queue = JSON.parse(memData.value.toString());
+    if (memData.value) queue = JSON.parse(memData.value.toString());
     return queue;
   }
 
   async emptyPrintQueue(namespace) {
     return await this.memcached.delete(namespace);
   }
-
 }
 
 module.exports = { BifrostPrinterStorage };
--- services/yures-printer-service.js
+++ services/yures-printer-service.js
@@ -1,13 +1,16 @@
-const { Socket } = require('socket.io');
-const { BifrostPrinterStorage } = require('../memcached/printer-storage');
-const { ConsoleLogger } = require('../util/console-logger');
+const { Socket } = require("socket.io");
+const { BifrostPrinterStorage } = require("../memcached/printer-storage");
+const { ConsoleLogger } = require("../util/console-logger");
 
 const EXPIRES_TIME_FROM_DATA = 300 * 60;
 const storage = new BifrostPrinterStorage(EXPIRES_TIME_FROM_DATA);
 const logger = new ConsoleLogger("YURES PRINTER SERVICE");
 
+const { Mutex } = require("async-mutex");
+const mutex = new Mutex();
+
 /**
- * @param {Socket} socket 
+ * @param {Socket} socket
  */
 function YuresPrinterService(socket) {
   const namespace = socket.nsp;
@@ -18,45 +21,77 @@ function YuresPrinterService(socket) {
     try {
       const storageInfo = await storage.enqueue(namespace.name, dataToPrint);
       if (storageInfo.success) {
-        logger.info(`Nuevo item guardado en cola de impresión en ${namespace.name}`);
+        logger.info(
+          `Nuevo item guardado en cola de impresión en ${namespace.name}`
+        );
         namespace.emit("printer:to-print", { data: storageInfo.memObject });
-        namespace.emit("yures:save-print-status", { message: "Impresion almacenada correctamente", success: "success" });
-      }
-      else {
-        logger.warning(`No se pudo persistir item en la cola de impresión en ${namespace.name}`);
-        namespace.emit("yures:save-print-status", { message: "No se persistio la información", success: "error" });
+        namespace.emit("yures:save-print-status", {
+          message: "Impresion almacenada correctamente",
+          success: "success",
+        });
+      } else {
+        logger.warning(
+          `No se pudo persistir item en la cola de impresión en ${namespace.name}`
+        );
+        namespace.emit("yures:save-print-status", {
+          message: "No se persistio la información",
+          success: "error",
+        });
       }
     } catch (error) {
-      namespace.emit("yures:save-print-status", { message: "El servidor a fallado, vuelve a intentarlo despues.", success: "error" });
-      logger.error(`Fallo en ejecución en el servidor en ${namespace.name}: ${error}`);
+      namespace.emit("yures:save-print-status", {
+        message: "El servidor a fallado, vuelve a intentarlo despues.",
+        success: "error",
+      });
+      logger.error(
+        `Fallo en ejecución en el servidor en ${namespace.name}: ${error}`
+      );
     }
-  })
-
+  });
 
   socket.on("printer:start", async (dataFromPrinter) => {
     try {
       const queue = await storage.getQueue(namespace.name);
-      namespace.emit("printer:load-queue", { message: "Se recupero los datos correctamente", data: queue, status: "success" });
-      logger.info(`Solicitud exitosa de extración de cola de impresión en ${namespace.name}`);
+      namespace.emit("printer:load-queue", {
+        message: "Se recupero los datos correctamente",
+        data: queue,
+        status: "success",
+      });
+      logger.info(
+        `Solicitud exitosa de extración de cola de impresión en ${namespace.name}`
+      );
     } catch (error) {
-      namespace.emit("printer:load-queue", { message: `El servidor tuvo un fallo: ${error.toString()}`, data: {}, status: "error" });
-      logger.error(`Solicitud FALLIDA de extración de cola de impresión en ${namespace.name}`);
+      namespace.emit("printer:load-queue", {
+        message: `El servidor tuvo un fallo: ${error.toString()}`,
+        data: {},
+        status: "error",
+      });
+      logger.error(
+        `Solicitud FALLIDA de extración de cola de impresión en ${namespace.name}`
+      );
     }
-  })
+  });
 
   socket.on("printer:printed", async (data) => {
-    try {
-      const success = await storage.dequeue(namespace.name, data.key);
-      if (success) {
-        logger.info(`Se libero un ticket de la cola de impresión en ${namespace.name}`);
-      } else {
-        logger.warning(`No se pudo liberar un ticket de la cola de impresión en ${namespace.name}`);
+    await mutex.runExclusive(async () => {
+      try {
+        const success = await storage.dequeue(namespace.name, data.key);
+        if (success) {
+          logger.info(
+            `Se libero un ticket de la cola de impresión en ${namespace.name}`
+          );
+        } else {
+          logger.warning(
+            `No se pudo liberar un ticket de la cola de impresión en ${namespace.name}`
+          );
+        }
+      } catch (error) {
+        logger.error(
+          `Error del servidor en ejecución en ${namespace.name}: ${error}`
+        );
       }
-    } catch (error) {
-      logger.error(`Error del servidor en ejecución en ${namespace.name}: ${error}`);
-    }
-  })
-
+    });
+  });
 }
 
 // yures:printer-{ruc}-{sufijo_sucursal}
@@ -64,5 +99,5 @@ const yuresPrinterNamespacePattern = /^\/yures:printer-(\d+)-([a-zA-Z]+)$/gm;
 
 module.exports = {
   YuresPrinterService,
-  yuresPrinterNamespacePattern
+  yuresPrinterNamespacePattern,
 };
